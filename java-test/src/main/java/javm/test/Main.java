86aad52c4b845288080a79d35d3a131a12ea9b22
XorTroll <xabofdez@gmail.com>
2021-08-24 21:25:26 +0200

    Rename test suite, add patreon link

--- java-test/src/main/java/javm/test/Main.java
+++
@@ -1,8 +0,0 @@
-package javm.test;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello world, hello KiVM.");
-        System.out.println("kivm.info: " + System.getProperty("kivm.info"));
-    }
-}
---
+++ javm-test-suite/src/main/java/javm/test/Main.java
@@ -0,0 +1,7 @@
+package javm.test;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world from libjavm!");
+    }
+}
